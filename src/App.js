f6d38a2ca43846d40951ea347abed5a584a8558f
cagdasaydemir <ac.cagdasaydemir@gmail.com>
2022-10-13 18:06:51 +0300

    setUp fontAwesome

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import "./App.scss";
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faCompass } from '@fortawesome/free-solid-svg-icons'
+  
 import Countries from "./components/Countries";
 import Filter from "./components/Filter";
 import Header from "./components/Header";
 
 function App() {
+  library.add( faCompass)
   return (
     <>
     <Header/>
     <Filter/>
-      <Countries />
+    <Countries />
     </>
   );
 }
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./index.scss";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const Header = () => {
   return (
@@ -7,10 +8,12 @@ const Header = () => {
       <header>
         <div>
             <h1>Where in the world?</h1>
+            <div>
+    </div>
         </div>
       
 <div>
-    icon
+<FontAwesomeIcon icon="fa-solid fa-compass" size="3x" />
 </div>
 
       </header>
